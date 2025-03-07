3735b1d7a864035b9fb77cf057d9b9e3327323f5
Filip Rokita <filiprokita4@gmail.com>
2025-03-07 23:28:29 +0100

    Add 433 MHz receiver functionality with RCswitch library

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
-#include <Arduino.h>
+// This code is for receiving signals from 433 MHz transmitter
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include <RCswitch.h>
+
+RCSwitch mySwitch = RCSwitch();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600); // Initialize serial port
+  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+  Serial.println("433 MHz Receiver is ready");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (mySwitch.available()) {
+    Serial.print("Received signal: ");
+    Serial.println(mySwitch.getReceivedValue());
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    mySwitch.resetAvailable(); // Reset the received value
+  }
 }
